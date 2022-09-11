862c1f692fa7f98e370d2b0e52debb9f1e42c2be
Movitz Sunar <movitz.sunar@gmail.com>
2022-09-11 15:49:53 +0200

    modified main class

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,4 @@ use crate::{board::Board};
 fn main() {
     let  p = Board::from_fen("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1");
     println!("{p:?}");
-    // p.taint_move();
-    println!("{p:?}");
 }
